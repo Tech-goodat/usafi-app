de3e00a06dac02603a9f99a9d307f466dd7574b8
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-02-18 15:12:07 +0300

    added homepage route for smaller screens in the sideNav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,17 +6,19 @@ import { FaBookReader } from "react-icons/fa";
 import { AiFillProject } from "react-icons/ai";
 import { MdContacts } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
+import { MdHome } from "react-icons/md";
 
 
 const SideNav = () => {
   return (
     <div className='flex flex-col gap-5   w-full h-full bg-gray-100 items-center'>
-      <div className='flex w-full items-center   mt-10 ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/about us'><IoInformationCircleOutline className='ml-2'/>About us</Link></div>
+      <div className='flex w-full items-center md:hidden   mt-10 ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/'><MdHome size={20} className="ml-2" />Home</Link></div>
+      <div className='flex w-full items-center   md:mt-10 ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/about us'><IoInformationCircleOutline className='ml-2'/>About us</Link></div>
       <div className='flex w-full items-center ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/Services'><LuClipboardCheck className='ml-2'/>Services</Link></div>
       <div className='flex w-full items-center ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/Careers'><FaBookReader className='ml-2'/>Careers</Link></div>
       <div className='flex w-full items-center ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/Sustainable projects'><AiFillProject className='ml-2'/>Sustainable projects</Link></div>
       <div className='flex w-full items-center ml-5  rounded-md   text-gray-600  font-bold text-sm'><Link className='flex w-full p-1 items-center hover:bg-green-200 hover:rounded-sm  hover:w-[180px] gap-2'  to='/contact'><MdContacts className='ml-2'/>Contact</Link></div>
-      <div className='flex w-full items-center ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex border border-gray-200 hover:shadow-none shadow-sm rounded-sm justify-center w-[180px] p-1 items-center hover:bg-green-300 hover:rounded-sm mt-[160px]  hover:w-[180px] gap-2'  to='/settings'><IoSettingsOutline className='ml-2'/>Settings</Link></div>
+      <div className='flex w-full items-center ml-5  rounded-md   text-gray-600 font-bold text-sm'><Link className='flex border border-gray-200 hover:shadow-none shadow-sm rounded-sm justify-center w-[180px] p-1 items-center hover:bg-green-300 hover:rounded-sm mt-[300px] lg:mt-[160px]  hover:w-[180px] gap-2'  to='/settings'><IoSettingsOutline className='ml-2'/>Settings</Link></div>
       
       
     </div>
