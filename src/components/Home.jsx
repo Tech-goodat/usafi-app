1f4af0108c5be9d9273a7d6033d6c146bb92d412
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-02-18 16:14:48 +0300

    done with desktop view for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
+import { FaVideo } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="flex flex-col items-center justify-center h-full  md:h-screen w-full">
+      <img src="/going green.png" alt="going green" className="w-[150px] mt-[60px]  md:mt-5 h-auto " />
+      <h1 className="text-3xl md:text-5xl text-gray-600 font-bold text-center">Zero-to-Hero <span className="text-green-600">Waste</span></h1>
+      <h1 className="text-3xl md:text-5xl font-bold text-center text-green-600">Management</h1>
+      <div className="flex w-full items-center justify-center"><p className="text-center text-sm font-bold w-full md:w-[700px] text-gray-600 mt-4">“Going Green” is our daily anthem and approach to Waste Management and Disposal Services through technological innovations that focuses on
+         sustainability, environment and safety.</p>
+      </div>
+      <button className="flex shadow-md cursor-pointer w-[200px] items-center justify-center text-white text-sm rounded-full bg-green-600 p-2 hover:bg-green-700 mt-6"><FaVideo size={20} className="mr-2"/>Watch Demo </button>
       
     </div>
   )
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -13,56 +13,59 @@ const TopNav = () => {
     const [input, setInput] = useState({ search: "" });
     const { darkMode, toggleDarkMode } = useTheme();
 
-    {/* Function to toggle the SideNav */ }
+    // Function to toggle the SideNav
     const toggleButtons = () => {
         setIsOpen(!isOpen);
     };
 
-    {/* Function to handle search input */ }
+    // Function to handle search input
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setInput({ ...input, [name]: value });
     };
 
     return (
-        <div className="w-full">
+        <div className="w-full overflow-x-hidden">
             {/* Top Navigation */}
-            <div className="flex fixed top-0 z-50 w-full h-[50px] bg-white items-center justify-between  px-5">
+            <div className="flex fixed top-0 z-50 w-full h-[50px] bg-white items-center justify-between px-5 max-w-screen">
                 {/* SideNav Toggle Button */}
-                <button onClick={toggleButtons} className="mr cursor-pointer">
+                <button onClick={toggleButtons} className="cursor-pointer">
                     {isOpen ? <IoIosClose size={25} /> : <FaBars size={18} />}
                 </button>
 
                 {/* Logo */}
-                <Link className='flex items-center justify-center' to="/">
-                    <img src="/usafi_plus-removebg-preview.png" alt="logo" className="h-auto w-[250px] ml-[14px] md:ml-0 sm:w-[150px] md:w-[55px] object-contain" />
+                <Link className="flex items-center justify-center" to="/">
+                    <img 
+                        src="/usafi_plus-removebg-preview.png" 
+                        alt="logo" 
+                        className="h-auto w-[150px] sm:w-[120px] md:w-[55px] object-contain" 
+                    />
                 </Link>
 
                 {/* Search Bar */}
-                <form className="flex items-center justify-between md:rounded-full md:border border-gray-200 w-[500px] px-3">
+                <form className="flex items-center justify-between md:rounded-full md:border border-gray-200 w-[500px] px-3 max-w-full">
                     <input
                         type="text"
                         name="search"
                         value={input.search}
                         onChange={handleInputChange}
-                        placeholder={window.innerWidth < 640 ? "" : "Search here..."}
+                        placeholder="Search here..."
                         className="text-[12px] bg-transparent outline-none sm:w-auto p-2 w-full"
                     />
-                    {window.innerWidth < 640 && <IoIosSearch size={25} className="text-green-700" />}
-                    <button className="cursor-pointer hidden md-block text-green-700">
+                    <button className="cursor-pointer hidden md:block text-green-700">
                         <IoIosSearch size={20} />
                     </button>
                 </form>
 
                 {/* Right Side Icons */}
-                <div className="flex items-center gap-6">
+                <div className="flex items-center gap-4">
                     <Link to="/"><MdHome size={20} className="hidden md:flex text-gray-600" /></Link>
                     <Link to="/notifications"><FaBell className="text-gray-600" /></Link>
                     <button className="text-gray-700 cursor-pointer" onClick={toggleDarkMode}>
                         {darkMode ? <MdOutlineDarkMode size={20} /> : <CiLight size={20} />}
                     </button>
                     <Link to="/login">
-                        <button className="lg:p-2 p-1 flex items-center justify-center md:w-[85px] w-[50px] rounded-sm md:rounded-md text-white text-sm bg-green-700">
+                        <button className="lg:p-2 p-1 flex hover:bg-green-700 items-center justify-center md:w-[85px] w-[50px] rounded-sm md:rounded-md text-white text-sm bg-green-600">
                             Login <CiLogin className="ml-2 hidden md:flex" size={19} />
                         </button>
                     </Link>
